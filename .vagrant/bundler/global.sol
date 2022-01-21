20fcc22b0fc1c5c05f3d4ca409beb9b824b41920
Rico Picone <ricopicone@gmail.com>
2022-01-20 19:38:14 -0800

    working

--- .vagrant/bundler/global.sol
+++ .vagrant/bundler/global.sol
@@ -1,1 +1,1 @@
-{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"40b2d2826507193788cdfd3711a0825df8ed639b7beeed55bc68dd617118b9b6","vagrant_version":"2.2.19"}
+{"dependencies":[["vagrant-reload",["= 0.0.1"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"4983392d70699ca412659bdbbbc644566e28ef81a344e7c9ef9d73f89c8f38c7","vagrant_version":"2.2.19"}
